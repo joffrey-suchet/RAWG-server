e67d77e950831b304cbfa4b21b692ce9d34329c9
joffrey-suchet <joffrey60s@gmail.com>
2022-08-17 14:38:00 +0200

    changed name of route

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("./home", async (req, res) => {
+router.get("/home", async (req, res) => {
   console.log("route home");
   try {
     axios
