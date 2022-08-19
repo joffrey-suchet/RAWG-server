6098d39df8fbb7e4538f59a018673eb8c948d598
joffrey-suchet <joffrey60s@gmail.com>
2022-08-19 18:38:03 +0200

    add key pagination

--- routes/home.js
+++ routes/home.js
@@ -4,32 +4,24 @@ const axios = require("axios");
 
 router.get("/home", async (req, res) => {
   console.log("route home");
-  console.log(req.query);
   try {
-    if (req.query.input) {
-      axios
-        .get(
-          `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${req.query.input}`
-        )
-        .then((response) => {
-          const home = response.data;
-          console.log(home);
-          res.status(200).json(home);
-        })
-        .catch((error) => {
-          console.log("catch from axios request", error);
-        });
-    } else {
-      axios
-        .get(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`)
-        .then((response) => {
-          const home = response.data;
-          res.status(200).json(home);
-        })
-        .catch((error) => {
-          console.log("catch from axios request", error);
-        });
-    }
+    const parent_platform = req.query.parent_platforms
+      ? req.query.parent_platforms
+      : "1,2,3,5,7";
+    const genres = req.query.genres
+      ? req.query.genres
+      : "1,2,3,4,5,6,7,8,9,10,12,13,14,15";
+    axios
+      .get(
+        `https://api.rawg.io/api/games?key=${process.env.API_KEY}&search=${req.query.search}&parent_platforms=${parent_platform}&genres=${genres}&page=${req.query.page}`
+      )
+      .then((response) => {
+        const home = response.data;
+        res.status(200).json(home);
+      })
+      .catch((error) => {
+        console.log("catch from axios request", error);
+      });
   } catch (error) {
     res.json({ message: "catch from home router" });
   }
