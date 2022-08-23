ce6097055408d6595ffc491e0d96255b1bcfecc7
joffrey-suchet <joffrey60s@gmail.com>
2022-08-23 13:25:08 +0200

    add signin route

--- routes/users.js
+++ routes/users.js
@@ -35,17 +35,48 @@ router.post("/home/signup", async (req, res) => {
 
         await newUser.save();
         console.log(newUser);
-        res.json({
+        res.status(200).json({
           _id: newUser._id,
           email: newUser.email,
+          username: newUser.username,
           token: newUser.token,
         });
-      } else if (isUserExisting !== null) {
-        res.json({ message: "the mail already" });
+      } else {
+        res.status(409).json({ message: "the mail already has an account" });
       }
     }
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+});
+
+// route pour se connecter à un compte
+
+router.post("/home/signin", async (req, res) => {
+  try {
+    const userToCheck = await User.findOne({ email: req.body.email });
+
+    if (userToCheck === null) {
+      res.status(401).json({ message: "unauthorized" });
+    } else {
+      const newHash = SHA256(req.body.password + userToCheck.salt).toString(
+        encBase64
+      );
+      console.log("newHash=>", newHash);
+      console.log("hash present en BDD=>", userToCheck.hash);
+
+      if (newHash === userToCheck.hash) {
+        res.json({
+          _id: userToCheck._id,
+          token: userToCheck.token,
+          username: userToCheck.username,
+        });
+      } else {
+        res.status(401).json({ message: "unauthorized" });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
